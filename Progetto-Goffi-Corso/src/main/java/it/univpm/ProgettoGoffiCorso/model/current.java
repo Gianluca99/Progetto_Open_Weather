d7e6c7ec20fee0abd0fd601acc673c0b9dfafd1e
AlessandroGoffi <76054341+AlessandroGoffi@users.noreply.github.com>
2020-12-17 16:41:02 +0100

    try

--- Progetto-Goffi-Corso/src/main/java/it/univpm/ProgettoGoffiCorso/model/current.java
+++ Progetto-Goffi-Corso/src/main/java/it/univpm/ProgettoGoffiCorso/model/current.java
@@ -1,5 +1,25 @@
 package it.univpm.ProgettoGoffiCorso.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
+//acquisizione dati della pressione attuale
 public class current {
+	String url = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+	HttpURLConnection OpenConnection = HttpURLConnection new URL(url).OpenCOnnection());
+	
 
 }
