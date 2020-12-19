e3c34c695b72e9595d413b7d9e8d32f80830c71e
Gianluca99 <s1089590@studenti.univpm.it>
2020-12-19 18:12:05 +0100

    Connessione OpenWeather

--- Progetto-Goffi-Corso/src/main/java/it/univpm/ProgettoGoffiCorso/Controller/Controller.java
+++ Progetto-Goffi-Corso/src/main/java/it/univpm/ProgettoGoffiCorso/Controller/Controller.java
@@ -1,7 +1,4 @@
 package it.univpm.ProgettoGoffiCorso.Controller;
-
- 
-
 import java.util.Map;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +15,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
@@ -31,45 +29,36 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.ParseException;
 	@RestController
-	public class Controller {{
-		//Creo richiesta di tipo http per PREVISIONI ATTUALI
-		try {
-			
+	public class Controller {
+		private static URLConnection connessione;
+		
+		
+		public static void main(String[] args) {
+		
+			String key = "f044a8c15896675617344a49813d1a16";
+			String città = "Ancona";
+			// TODO Auto-generated method stub
+			try {
+				StringBuilder result = new StringBuilder();
+				URL url = new URL ("api.openweathermap.org/data/2.5/weather?q="+ città + "&appid="+ key);
+				connessione = url.openConnection();
+				BufferedReader rd = new BufferedReader (new InputStreamReader(connessione.getInputStream()));
+				String line;
+				while((line = rd.readLine()) != null) {
+					result.append(line);
+				}
+				rd.close();
+				System.out.println(result);
+				//int lung = connessione.getContentLength();
+				int tempo= connessione.getConnectTimeout();
+				System.out.println(tempo);
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
 
-			String url = "api.openweathermap.org/data/2.5/weather?q=Ancona&appid=f044a8c15896675617344a49813d1a16";
-			URLConnection openConnection = new URL(url).openConnection();
-			InputStream in = openConnection.getInputStream();
-			//Risposta da parte del server
-			String risposta = new Scanner(in).next();
-			BufferedWriter dati = new BufferedWriter(new FileWriter ("dati.txt"));
-			while(risposta.isEmpty() == false) {
-				dati.write(risposta);}
-			
-				dati.close();
-			
-				//InputStreamReader inR = new InputStreamReader( in ); 
-				//BufferedReader buf = new BufferedReader( writer );
-				/*
-				while((line = buf.readLine()) != null) {
-					data += line;
-					System.out.println(line);
-				}*/
-				in.close();
-				
-		
-		}
-		catch(IOException e) {
-			System.out.println("Errore nella lettura.");
-			System.out.println(e);
-		}
-		
-		JSONObject obj = new JSONObject();
-		System.out.println("OK");
-		}
-		
-		
-		}
-			
+		}}
+
+	
 
         
         
